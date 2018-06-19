971de90f5bd24b4f5ac135003e9fc3759cf5fbc6
Moumita Kanrar <40354624+moumitakanrar@users.noreply.github.com>
2018-06-19 19:32:13 +0530

    Update process.php

--- process.php
+++ process.php
@@ -6,14 +6,23 @@ $email = $_POST["email_id"];
 $phNo = $_POST["ph_no"];
 $country = $_POST["country"];
 
-include("_inc/dbConnection.php");
-$sql = "INSERT INTO `my_user`( `namew`, `email`, `phone_no`, `country`) VALUES ('$name','$email','$phNo','$country')";
+include("dbConnection.php");
+$sql = "INSERT INTO `my_user`( `name`, `email`, `phone_no`, `country`) VALUES ('$name','$email','$phNo','$country')";
 
 $result = mysqli_query($conn,$sql) or die("Error: ".mysqli_error($conn));
 
-if($result>0){
-echo "you have successfully registerred.";
-header("refresh:5;url=index.php");
+if($result){
+
+	if(mysqli_affected_rows($conn)>0){
+         echo "Your data has been inserted successfully.";
+         header("refresh:5;url=index.php");
+                     
+        }else{
+		 echo "Fail to insert.";
+         header("refresh:5;url=index.php");
+
+        }
+
 }else{
 	echo "Something went wrong";
 	header("refresh:5;url=index.php");
@@ -31,14 +40,24 @@ $email = $_POST["email_id"];
 $phNo = $_POST["ph_no"];
 $country = $_POST["country"];
 
-include("_inc/dbConnection.php");
+include("dbConnection.php");
 $sql = "UPDATE `my_user` SET `name`='$name',`email`='$email',`phone_no`='$phNo',`country`='$country' WHERE id='$id'";
 
 $result = mysqli_query($conn,$sql) or die("Error: ".mysqli_error($conn));
 
-if($result>0){
-echo "data have been successfully updated.";
-header("refresh:5;url=view-data.php");
+if($result){
+
+	if(mysqli_affected_rows($conn)>0){
+         echo "Your data has been updated successfully.";
+         header("refresh:5;url=view-data.php");
+                     
+        }else{
+		 echo "Fail to update.";
+         header("refresh:5;url=view-data.php");
+
+        }
+
+
 }else{
 	echo "Something went wrong!";
 	header("refresh:5;url=view-data.php");
