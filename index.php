b71f7073661fc16c9640556e48e5bad764bccd3b
Moumita Kanrar <40354624+moumitakanrar@users.noreply.github.com>
2018-06-19 12:45:23 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,33 @@
+<html>
+	<head>
+		<title>test</title>
+	</head>
+	<body>
+	<form action="process.php" method="post" >
+	<table>
+			<tr>
+				<td>Name:</td>
+				<td><input type="text" name="name"></td>
+			</tr>
+			<tr>
+				<td>Email:</td>
+				<td><input type="email" name="email_id"></td>
+			</tr>
+			<tr>
+				<td>Phone No:</td>
+				<td><input type="number" name="ph_no"></td>
+			</tr>
+			<tr>
+				<td>Country:</td>
+				<td><input type="text" name="country"></td>
+			</tr>
+			<tr>
+				
+				<td colspan="2" align="right"><input type="submit" name="submitBtn"></td>
+			</tr>
+		</table>
+		
+	</form>
+		
+	</body>
+</html>
