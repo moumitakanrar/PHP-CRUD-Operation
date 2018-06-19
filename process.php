8e88d3c38830e4cf7a7212919f066bc3a6280c2e
Moumita Kanrar <40354624+moumitakanrar@users.noreply.github.com>
2018-06-19 12:49:27 +0530

    Create process.php

---
+++ process.php
@@ -0,0 +1,25 @@
+<?php
+
+if(isset($_POST["submitBtn"])){
+$name = $_POST["name"];
+$email = $_POST["email_id"];
+$phNo = $_POST["ph_no"];
+$country = $_POST["country"];
+
+include("dbConnection.php");
+$sql = "INSERT INTO `my_user`( `name`, `email`, `phone_no`, `country`) VALUES ('$name','$email','$phNo','$country')";
+
+$result = mysqli_query($conn,$sql);
+
+if($result>0){
+echo "you have successfully registerred.";
+header("refresh:5;url=index.php");
+}
+
+
+
+
+}
+
+
+?>
