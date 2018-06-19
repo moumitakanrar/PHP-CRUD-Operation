700a63abfda19c89fca9de351b2f30f3b0c15c81
Moumita Kanrar <40354624+moumitakanrar@users.noreply.github.com>
2018-06-19 13:14:07 +0530

    Update process.php

--- process.php
+++ process.php
@@ -6,7 +6,7 @@ $email = $_POST["email_id"];
 $phNo = $_POST["ph_no"];
 $country = $_POST["country"];
 
-include("dbConnection.php");
+include("_inc/dbConnection.php");
 $sql = "INSERT INTO `my_user`( `name`, `email`, `phone_no`, `country`) VALUES ('$name','$email','$phNo','$country')";
 
 $result = mysqli_query($conn,$sql);
@@ -14,12 +14,18 @@ $result = mysqli_query($conn,$sql);
 if($result>0){
 echo "you have successfully registerred.";
 header("refresh:5;url=index.php");
+}else{
+	echo "Something went wrong";
+	header("refresh:5;url=index.php");
+}
+
+mysqli_close($conn);
+
+
+
 }
 
 
-
-
-}
 
 
 ?>
