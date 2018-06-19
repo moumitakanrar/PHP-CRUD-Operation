6e2de749338562ca65736777122314af44294de1
Moumita Kanrar <40354624+moumitakanrar@users.noreply.github.com>
2018-06-19 14:30:44 +0530

    Update process.php

--- process.php
+++ process.php
@@ -7,9 +7,9 @@ $phNo = $_POST["ph_no"];
 $country = $_POST["country"];
 
 include("_inc/dbConnection.php");
-$sql = "INSERT INTO `my_user`( `name`, `email`, `phone_no`, `country`) VALUES ('$name','$email','$phNo','$country')";
+$sql = "INSERT INTO `my_user`( `namew`, `email`, `phone_no`, `country`) VALUES ('$name','$email','$phNo','$country')";
 
-$result = mysqli_query($conn,$sql);
+$result = mysqli_query($conn,$sql) or die("Error: ".mysqli_error($conn));
 
 if($result>0){
 echo "you have successfully registerred.";
@@ -24,8 +24,31 @@ mysqli_close($conn);
 
 
 }
+else if(isset($_POST["updateBtn"])){
+$id = $_POST['id'];
+$name = $_POST["name"];
+$email = $_POST["email_id"];
+$phNo = $_POST["ph_no"];
+$country = $_POST["country"];
+
+include("_inc/dbConnection.php");
+$sql = "UPDATE `my_user` SET `name`='$name',`email`='$email',`phone_no`='$phNo',`country`='$country' WHERE id='$id'";
+
+$result = mysqli_query($conn,$sql) or die("Error: ".mysqli_error($conn));
+
+if($result>0){
+echo "data have been successfully updated.";
+header("refresh:5;url=view-data.php");
+}else{
+	echo "Something went wrong!";
+	header("refresh:5;url=view-data.php");
+}
+
+mysqli_close($conn);
 
 
+
+}
 
 
 ?>
